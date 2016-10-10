450d84abbaf6a49d48a1bf125249464e6b63b498
lianxinzhong <macmini4@XingGudeMac-mini-4.local>
2016-10-10 20:04:33 +0800

    add TextFileText example

---
+++ corejava_v2_ch01/src/main/java/com/core/java/TextFileTest/TextFileTest.java
@@ -0,0 +1,15 @@
+package com.core.java.TextFileTest;
+
+/**
+ * Test Text File
+ *
+ * @auth 	Lian
+ * @date 	2016/10/10
+ * @since 	1.0
+ */
+public class TextFileTest {
+
+	public static void main(String[] args) {
+
+	}
+}
